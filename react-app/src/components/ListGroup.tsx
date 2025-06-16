617ff2452d20d48bfc87c65a7374e5f32c83f28d
YashdeepN <yashdeepneema@hotmail.com>
2025-06-16 00:48:49 -0500

    conditional rendering ListGroup element

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -2,14 +2,21 @@ import React from "react";
 
 const ListGroup = () => {
   const items = ["London", "New Delhi", "Tokyo", "Mumbai"];
+
   return (
     <>
       <h1>List Group</h1>
-      <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item"> {item}</li>
-        ))}
-      </ul>
+      {items.length === 0 ? (
+        <p>No items found!</p>
+      ) : (
+        <ul className="list-group">
+          {items.map((item) => (
+            <li key={item} className="list-group-item">
+              {item}
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 };
