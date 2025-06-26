69aabc366d3a8db0c1e0068dd96e14b5458b4fde
YashdeepN <yashdeepneema@hotmail.com>
2025-06-26 07:29:41 -0500

    expense tracker initial commit

---
+++ react-app/src/components/expense-tracker/components/MyApp.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ExpenseList from "./ExpenseList";
+
+const expenses = [
+  { id: 0, description: "item1", amount: 3, category: "some" },
+  { id: 1, description: "item2", amount: 1, category: "some" },
+  { id: 2, description: "item3", amount: 5, category: "some" },
+  { id: 3, description: "item4", amount: 13, category: "some" },
+];
+
+const MyApp = () => {
+  return (
+    <>
+      <ExpenseList expenses={expenses} />
+    </>
+  );
+};
+
+export default MyApp;
--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
+import MyApp from "./components/expense-tracker/components/MyApp.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <MyApp />
   </StrictMode>
 );
