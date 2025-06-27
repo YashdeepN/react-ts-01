800709717c15bbea71a6787121915e1137e4b03a
YashdeepN <yashdeepneema@hotmail.com>
2025-06-26 21:46:18 -0500

    Expense Filter markup

--- react-app/src/components/expense-tracker/components/ExpenseFilter.tsx
+++ react-app/src/components/expense-tracker/components/ExpenseFilter.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const ExpenseFilter = () => {
-  return <div>ExpenseFilter</div>;
+  return (
+    <>
+      <select className="form-select">
+        <option value="">All Categories</option>
+        <option value="">Groceries</option>
+        <option value="">Utilities</option>
+        <option value="">Entertainment</option>
+      </select>
+    </>
+  );
 };
 
 export default ExpenseFilter;
