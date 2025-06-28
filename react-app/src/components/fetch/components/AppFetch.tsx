38760c2a3362bb59eb0f1f24f5420e6507156f89
YashdeepN <yashdeepneema@hotmail.com>
2025-06-27 21:52:46 -0500

    fetch data using axios

--- react-app/src/components/fetch/components/AppFetch.tsx
+++ react-app/src/components/fetch/components/AppFetch.tsx
@@ -1,6 +1,20 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 const AppFetch = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  }, []);
+
   return <div>AppFetch</div>;
 };
 
