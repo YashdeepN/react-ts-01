080a119e58274cb5d9f5698b765f2f91c4cf522a
YashdeepN <yashdeepneema@hotmail.com>
2025-06-27 22:04:15 -0500

    handle error in fetching

--- react-app/src/components/fetch/components/AppFetch.tsx
+++ react-app/src/components/fetch/components/AppFetch.tsx
@@ -8,15 +8,18 @@ interface User {
 
 const AppFetch = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
+      .then((res) => setUsers(res.data))
+      .catch((err) => setError(err.message));
   }, []);
 
   return (
     <>
+      <p className="text-danger">{error}</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
