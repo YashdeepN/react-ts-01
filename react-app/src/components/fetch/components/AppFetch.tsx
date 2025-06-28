6e30fd84f8b7bd8478c40db71e6ca97aec6e4046
YashdeepN <yashdeepneema@hotmail.com>
2025-06-27 21:32:08 -0500

    Fetch data initial commit

---
+++ react-app/src/components/fetch/components/AppFetch.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AppFetch = () => {
+  return <div>AppFetch</div>;
+};
+
+export default AppFetch;
--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -4,10 +4,12 @@ import App from "./App.tsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import MyApp from "./components/expense-tracker/components/MyApp.tsx";
+import AppFetch from "./components/fetch/components/AppFetch.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <App /> */}
+    <AppFetch />
+    <App />
     <MyApp />
   </StrictMode>
 );
